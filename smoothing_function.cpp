31634161baeeef1a126c7184a1673e963b6e9c56
Mark <markshif1@gmail.com>
2024-04-27 13:50:34 +0300

    new smooth

--- smoothing_function.cpp
+++ smoothing_function.cpp
@@ -1,7 +1,17 @@
 #include<iostream>
 #include<vector>
 
-void smooth(std::vector<int>& arr){
+void smooth_easy(std::vector<int>& arr){
     for(int i = 1; i<arr.size(); ++i)
         arr[i] = (arr[i-1] + arr[i]) / 2;
+}
+
+void smooth(std::vector<int>& arr){
+    std::vector<int> rhs = arr;
+    int n = arr.size();
+    for(int i = 0; i < n - 2; ++i){
+        int j = (i == n - 1) ? 0 : i + 1;
+        rhs[i] = 4 * arr[i] + 2 * arr[j];
+    }
+    arr = rhs;
 }
